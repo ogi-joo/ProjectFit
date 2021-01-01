fa5eeeba72a0b05eb425ac79c38322a016373f17
Ognjen Jovanovic <jovanovicogi@yandex.com>
2021-01-01 12:48:24 +0100

    :pig: sample

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('ya');
+    res.send('f');
+});
+
+mongoose.connect('mongodb://root:pJ]9ReK{vW1HQWBo@136.244.81.175:27017/?authSource=admin', () => {
+    console.log('connected')
 });
 
 app.listen('3000')
